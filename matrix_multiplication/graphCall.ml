eac2864a26984f7698343d5f9b09a38c4ba5e2cf
Sergiy Vakulenko <svakulenko.a@gmail.com>
2012-02-18 19:13:21 +0100

    matrix multiplication comment printfs

--- matrix_multiplication/graphCall.ml
+++ matrix_multiplication/graphCall.ml
@@ -26,6 +26,10 @@ let print_relation x =
     | Unknown ->  printf "Unknown " 
     | InitStateOfVariable ->  printf "InitStateOfVariable " 
 
+
+exception Exception_dimensions_not_equals;;
+exception Exception_cant_multiply;;
+
 let print_relation_list x =
     printf "\nmatrix data length = %d" (List.length x.data);
     printf "  dimention_x size=%d; dimention_y size=%d\n\n" x.d_x x.d_y ;
@@ -56,19 +60,6 @@ let mult relations =
     | (x, InitStateOfVariable ) -> x
 
 
-
-
-
-(*let multiplication matrices =*)
-
-
-exception Exception_dimensions_not_equals;;
-exception Exception_cant_multiply;;
-
-let print_list x = 
-    List.iter (fun x -> printf "lst element:%d\n" x) x;;
-
-
 let multiplication m1 m2 = 
 
     if m1.d_y == m2.d_x then
@@ -77,17 +68,17 @@ let multiplication m1 m2 =
         let matrix_of_product = ref [] in 
         (*printf "m1.d_x=%d; m2.d_y=%d\n" m1.d_x m2.d_y ;*)
         for i=0 to len_y do
-            printf "i=%d -------------- next ----------\n" i;
+            (*printf "i=%d -------------- next ----------\n" i;*)
             for j=0 to len_y do
                 let add_mult = ref InitStateOfVariable in
                 for k=0 to len_y do
                     let m1_index = (i*len_x) + k in
                     let m2_index = (k*len_x) + j in
-                    printf "m1_index=%d, m2_index=%d\n" m1_index m2_index ;
+                    (*printf "m1_index=%d, m2_index=%d\n" m1_index m2_index ;*)
                     let add_res  = mult (List.nth m1.data m1_index,List.nth m1.data m2_index) in
                     add_mult    := add (add_mult.contents,add_res) ;
                 done;
-                printf "-------------- save produit ----------\n";
+                (*printf "-------------- save produit ----------\n";*)
                 matrix_of_product :=  insert_at_end !matrix_of_product !add_mult;
             done
         done;
@@ -106,6 +97,7 @@ let multiplication m1 m2 =
 
 
 (*------TEST 1-------------------------------------------------------------*)
+
 (*let m1 = { data = [Inf; Inf; Inf;*)
                    (*Inf; Inf; Inf;*)
                    (*Inf; Inf; Inf] ; d_x = 3; d_y = 3 };;*)
@@ -116,6 +108,7 @@ let multiplication m1 m2 =
 (*print_relation_list v;;*)
 
 (*------TEST 2-------------------------------------------------------------*)
+
 (*let m1 = { data = [Inf; Inf; Inf; Inf;*)
                    (*Inf; Inf; Inf; Inf;*)
                    (*Inf; Inf; Inf; Inf] ; d_x = 4; d_y = 3 };;*)
@@ -127,6 +120,7 @@ let multiplication m1 m2 =
 (*print_relation_list v;;*)
 
 (*------TEST 3-------------------------------------------------------------*)
+
 (*let m1 = { data = [Inf; Inf; Inf;*)
                    (*Inf; Inf; Inf;*)
                    (*Inf; Inf; Inf;*)
