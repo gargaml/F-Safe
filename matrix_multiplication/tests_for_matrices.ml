cd4c6fe9fa14b40c1de3d9372424b5b68c44402e
Thierry <thierry.paulin@etu.upmc.fr>
2012-02-24 13:53:18 +0100

    Encore des matrices...

--- matrix_multiplication/tests_for_matrices.ml
+++ matrix_multiplication/tests_for_matrices.ml
@@ -1,3 +1,12 @@
+(*****************************************************************************)
+(*                                                                           *)
+(* F-Safe                                                                    *)
+(*                                                                           *)
+(* File        : Tests_for_matrices.ml                                       *)
+(* Description : Tests for matrices                                          *)
+(*                                                                           *)
+(*****************************************************************************)
+
 open Printf
 
 open Matrix
@@ -5,6 +14,12 @@ open Matrix
 open Product_of_matrices
 
 open Print_of_matrices
+
+type result = Passed | Failed ;;
+
+type expectation = OK | KO of exn * string ;;
+
+let _MAX_TESTS = 10 ;;
 
 let m1 = { data = [ Inf ;
                     Eq ;
@@ -70,44 +85,107 @@ let m8 = { data = [Eq; Eq; Eq; Eq;
          }
 ;;
 
-let test1 () =
-  printf "Test n° 1\n" ;
+(* num    : the number of the test *)
+(* m1     : the 1st matrix         *)
+(* m2     : the 2nd matrix         *)
+(* expect : the expect result      *)
+let test num m1 m2 expect =
+  printf "\nTest n° %d\n" (num + 1) ;
   printf "Display of m1\n" ;
   print_matrix m1 ;
   printf "Display of m2\n" ;
   print_matrix m2 ;
   printf "Display of m1 × m2\n" ;
-  print_matrix (multiplication m1 m2)
+  match expect with
+    OK ->
+      begin
+        try
+          print_matrix (multiplication m1 m2) ;
+          Passed ;
+        with _ -> Failed ;
+      end
+  | KO(excep, message) ->
+      begin
+        try
+          print_matrix (multiplication m1 m2) ;
+          Failed ;
+        with excep ->
+          printf "%s\n" message ;
+          Passed ;
+      end
 ;;
 
-let test2 () =
-  printf "Test n° 2\n" ;
-  printf "Display of m2\n" ;
-  print_matrix m2 ;
-  printf "Display of m1\n" ;
-  print_matrix m1 ;
-  printf "Display of m1 × m2\n" ;
-  print_matrix (multiplication m2 m1)
-;;
-
-let test3 () =
-  printf "Test n° 3\n" ;
-  printf "Display of m1\n" ;
-  print_matrix m1 ;
-  printf "Display of m3\n" ;
-  print_matrix m3 ;
-  printf "Display of m1 × m3\n" ;
-  try
-    print_matrix (multiplication m1 m3)
-  with Exception_incompatible_dimensions ->
-    printf "Matrices are not compatible.\n"
+let print_result res =
+  match res with
+    Passed -> printf "PASSED" ;
+  | Failed -> printf "FAILED" ;
 ;;
 
 let main () =
-	printf "Début des tests...\n" ;
-	test1 () ;
-	test2 () ;
-	test3 () ;
+  printf "Start of tests...\n" ;
+  let results = Array.make _MAX_TESTS Failed in
+    let number = ref 0 in
+      Array.set results
+        !number
+        (test !number m1 m2 OK)
+      ;
+      incr number ;
+      Array.set results
+        !number
+        (test !number m2 m1 OK)
+      ;
+      incr number ;
+      Array.set results
+        !number
+        (test
+          !number
+          m1
+          m3
+          (KO(
+            Exception_incompatible_dimensions,
+            "Matrices are not compatible."
+          ))
+        )
+      ;
+      incr number ;
+      Array.set results
+        !number
+        (test !number m2 m4 OK)
+      ;
+      incr number ;
+      Array.set results
+        !number
+        (test !number m2 m5 OK)
+      ;
+      incr number ;
+      Array.set results
+        !number
+        (test
+          !number
+          m1
+          m6
+          (KO(
+            Exception_incompatible_dimensions,
+            "Matrices are not compatible."
+          ))
+        )
+      ;
+      incr number ;
+      Array.set results
+        !number
+        (test !number m6 m1 OK)
+      ;
+      incr number ;
+      Array.set results
+        !number
+        (test !number m7 m8 OK)
+      ;
+      printf "\n\nSummary : \n" ;
+      for i = 0 to !number do
+        printf "Test n° %d... " (i+1) ;
+        print_result (Array.get results i) ;
+        printf "\n" ;
+      done ;
 ;;
 
 main ()
