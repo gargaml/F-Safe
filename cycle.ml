a6e8314df2278a6497be12934ab76d56f332e9ee
Vince <vince@MacBucheOne.(none)>
2012-03-15 00:30:39 +0100

    now get only inner cycle for, each beginning function has a list of cycle

--- cycle.ml
+++ cycle.ml
@@ -53,6 +53,40 @@ let get_all_cycles callgraph mainfunc =
   in
   List.flatten cycles_list
 
+
+let rec inner_explore_couple callgraph cycle history = function
+  | funname, edges ->
+    match edges with 
+      | [] -> []
+      | edge :: rest -> 
+	let rec explore_edge fun_name edge callgraph cycle history =
+	  let rec filter_cycle fname = function
+	    | (calledname,edge)::rest ->  if (String.compare calledname fname) == 0 then (fname,edge)::rest else filter_cycle fname rest
+	    |[] -> failwith "error here"
+	  in
+	  match edge with 
+	    | (calledname,_,_,_) ->
+	      let next_cycle = cycle @ [(fun_name,edge)] in
+	      let next_edges = CallGraph.find calledname callgraph in
+	      let next_cycles = calledname, next_edges in
+	      if List.mem calledname history then
+		[filter_cycle calledname next_cycle]
+	      else
+		inner_explore_couple callgraph next_cycle history next_cycles
+	in
+	explore_edge funname edge callgraph cycle (funname::history)
+	@ inner_explore_couple  callgraph cycle (funname::history) (funname, rest)
+
+
+let get_all_inner_cycles callgraph mainfunc =   let main_callgraphs =
+    List.map (fun f -> (f,CallGraph.find f callgraph)) mainfunc in
+  let cycles_list = List.map 
+    (fun x ->match x with | (f,_) ->
+(f, (inner_explore_couple  callgraph [] [] x )))
+    main_callgraphs
+  in
+    cycles_list
+
 let rec string_of_cyclelist cl =
   let rec string_of_cycle cycle = 
     match cycle with
@@ -63,9 +97,14 @@ let rec string_of_cyclelist cl =
 	calling ^ " -> " ^ called ^ " , " ^ (string_of_cycle rest)
   in
   match cl with
-    | [] -> " }"
+    | [] -> ""
     | cycle :: rest -> "(" ^ (string_of_cycle cycle) ^ (string_of_cyclelist rest)
       
+
+let rec  string_of_inner = function 
+  | [] -> "no cycle"
+  | [(fname, cycle)] -> fname ^ " => { " ^ (string_of_cyclelist cycle) ^ " }"
+  | (fname, cycle)::rest-> fname ^ " => { " ^ (string_of_cyclelist cycle) ^ " } \n"  ^ (string_of_inner rest)
 let cycletest = 
   let cycletotest = 
     CallGraph.add 
@@ -82,7 +121,7 @@ let cycletest =
       ) 
   in
   let mainfuns = ["f";"h"] in
-  printf "il y a ces cycles dans le programme : %s\n"
-    (string_of_cyclelist (get_all_cycles cycletotest mainfuns))
+  printf "il y a ces cycles dans le programme : %s\n ou ou autrement dit: %s\n"
+    (string_of_cyclelist (get_all_cycles cycletotest mainfuns)) (string_of_inner (get_all_inner_cycles cycletotest mainfuns))
      
      
