61ff1bf44e85e6bec17d7305e27fe7b2bc9e1ac5
ad0 <aurelien.deharbe@gmail.com>
2012-03-19 01:24:10 +0100

    Coding conventions

--- relationmatrix.ml
+++ relationmatrix.ml
@@ -30,9 +30,11 @@ type relation =
 type relationmatrix = 
     { mutable data : relation array array ; nb_c : int ; nb_l : int }
 
+(* empty : int -> int -> relationmatrix *)
 let empty c l = 
   { data = Array.create_matrix c l Unknown ; nb_c = c ; nb_l = l }
 
+(* add : relation -> relation -> relation *)
 let add relations =
   match relations with
     | (Inf, _) | (_, Inf) -> Inf
@@ -40,6 +42,7 @@ let add relations =
     | (Unknown, Eq) -> Eq
     | (Unknown, Unknown) -> Unknown
 
+(* mult : relation -> relation -> relation *)
 let mult relations =
   match relations with
     | (Unknown, _) | (_, Unknown) -> Unknown
@@ -47,37 +50,39 @@ let mult relations =
     | (Eq, Inf) -> Inf
     | (Eq, Eq) -> Eq
 
-(* Mutliplication of two matrices of relations *)
+(* multiplication of two matrices of relations *)
+(* multiplication : relationmatrix -> relationmatrix -> relationmatrix *)
 let multiplication m1 m2 = 
   if m1.nb_c == m2.nb_l then
     let lim_i = (m1.nb_l - 1) in
-      let lim_j = (m2.nb_c - 1) in
-        let lim_k = (m1.nb_c - 1) in
-          let matrix_product = 
-	    ref (Array.make_matrix m1.nb_l m2.nb_c Unknown) in 
-            for i = 0 to lim_i do
-              let line = ref (Array.make m2.nb_c Unknown) in 
-                for j = 0 to lim_j do
-                  let add_mult = ref Unknown in
-                    (* Because Unknown is the neutral element for addition *)
-                    for k = 0 to lim_k do
-                      let add_res  = mult (m1.data.(i).(k), m2.data.(k).(j)) in
-                        add_mult  := add (add_mult.contents,add_res) ;
-                    done ;
-                    Array.set !line j !add_mult ;
-                done ;
-                Array.set !matrix_product i !line ;
-            done ;
-            { data = !matrix_product ; nb_l = m1.nb_l ; nb_c = m2.nb_c } ;
+    let lim_j = (m2.nb_c - 1) in
+    let lim_k = (m1.nb_c - 1) in
+    let matrix_product = 
+      ref (Array.make_matrix m1.nb_l m2.nb_c Unknown) in 
+    for i = 0 to lim_i do
+      let line = ref (Array.make m2.nb_c Unknown) in 
+      for j = 0 to lim_j do
+        let add_mult = ref Unknown in
+        for k = 0 to lim_k do
+          let add_res  = mult (m1.data.(i).(k), m2.data.(k).(j)) in
+          add_mult  := add (add_mult.contents,add_res) ;
+        done ;
+        Array.set !line j !add_mult ;
+      done ;
+      Array.set !matrix_product i !line ;
+    done ;
+    { data = !matrix_product ; nb_l = m1.nb_l ; nb_c = m2.nb_c } ;
   else
     raise Exception_incompatible_dimensions
       
+(* string_of_relation : relation -> string *)
 let string_of_relation x = 
   match x with
     | Inf     -> "< "
-    | Eq      -> "= " 
-    | Unknown -> "? " 
-	
+    | Eq      -> "= "
+    | Unknown -> "? "
+
+(* string_of_relationmatrix : relationmatrix -> string *)
 let string_of_relationmatrix m =
   let accu = ref "" in
     for i = 0 to m.nb_l - 1 do
@@ -88,6 +93,7 @@ let string_of_relationmatrix m =
     done;
     !accu
 
+(* string_of_relationmatrix' : relationmatrix -> string *)
 let string_of_relationmatrix' m =
   let accu = ref "" in
   for i = 0 to m.nb_l - 1 do
@@ -99,4 +105,3 @@ let string_of_relationmatrix' m =
     accu := !accu ^ "\\n"
   done;
   !accu
-    
