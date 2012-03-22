2995e794f1d8951baee2cb5f965d82dfa714dc52
Jeremie Salvucci <jeremie.salvucci@free.fr>
2012-03-22 10:07:26 +0100

    Decoration of the AST.

--- typechecker.ml
+++ typechecker.ml
@@ -27,16 +27,8 @@ let decorate_ast dcenv { types = ts; globals = gs; entry = es } =
   let rec decorate aenv te =
     match te.e with
       | EVar v -> { e = EVar v; t = Some (SMap.find v aenv) }
-      | EConApp (dc, ans, tes) -> (
-	match SMap.find dc dcenv with
-	  | TScheme (_, _, a) -> (
-	    match a with
-	      | (AConApp (tc, _)) as a ->
-		let tes' = List.map (decorate aenv) tes in
-		{ e = EConApp (dc, ans, tes'); t = Some a }
-	      | _ -> failwith "Unexpected annotation"
-	  )
-      )
+      | EConApp (dc, ans, tes) ->
+	decorate_econapp aenv dc ans tes
       | ELet (tvtes, tes) ->
 	let f (tv, te) = (tv, decorate aenv te) in
 	let add acc ((v, a), _) = SMap.add v a acc in
@@ -65,7 +57,17 @@ let decorate_ast dcenv { types = ts; globals = gs; entry = es } =
 	let ps' = decorate_patterns aenv ps in
 	match List.hd ps' with
 	  | Pattern (_, { e = _; t = ao }) -> { e = ECase (tes', ps'); t = ao }
-	    
+
+  and decorate_econapp aenv dc ans tes =
+    match SMap.find dc dcenv with
+      | TScheme (_, _, a) -> (
+	match a with
+	  | (AConApp (tc, _)) as a ->
+	    let tes' = List.map (decorate aenv) tes in
+	    { e = EConApp (dc, ans, tes'); t = Some a }
+	  | _ -> failwith "Unexpected annotation"
+      )
+
   and decorate_patterns aenv ps =
     let rec add_filters aenv = function
       | [] -> aenv
@@ -89,25 +91,34 @@ let decorate_ast dcenv { types = ts; globals = gs; entry = es } =
       | GDef ((v, t), te) ->
 	let te' = decorate aenv te in (
 	match te' with
-	  | { e = _; t = Some a } ->
-	    let aenv' = SMap.add v a aenv in
-	    let d' = GDef ((v, t), te') in
-	    aenv', d'
+	  | { e = _; t = Some a } -> GDef ((v, t), te')
 	  | _ -> failwith "Unexpected..."
 	)
       | GRecDef (tvs, te) -> failwith "Not yet..."
   in
   let decorate_definitions aenv gs =
-    let rec loop aenv globals = function
-      | [] -> aenv, List.rev globals
+    let rec loop globals = function
+      | [] -> List.rev globals
       | d :: ds ->
-	let aenv', d' = decorate_definition aenv d in
-	loop aenv' (d' :: globals) ds
-    in loop aenv [] gs
+	let d' = decorate_definition aenv d in
+	loop (d' :: globals) ds
+    in loop [] gs
   in
   let decorate_expressions aenv es =
     List.map (decorate aenv) es
   in
-  let aenv, gs' = decorate_definitions SMap.empty gs in
+  let init aenv =
+    let rec loop aenv gs = match gs with
+      | [] -> aenv
+      | GDef ((v, t), _) :: gs ->
+	let aenv' = SMap.add v t aenv in
+	loop aenv' gs
+      | GRecDef (tvs, _) :: gs ->
+	let aenv' = List.fold_left (fun acc (v, t) -> SMap.add v t acc) aenv tvs in
+	loop aenv' gs
+    in loop aenv gs
+  in
+  let aenv = init SMap.empty in
+  let gs' = decorate_definitions SMap.empty gs in
   let es' = decorate_expressions aenv es in
   { types = ts; globals = gs'; entry = es' }
